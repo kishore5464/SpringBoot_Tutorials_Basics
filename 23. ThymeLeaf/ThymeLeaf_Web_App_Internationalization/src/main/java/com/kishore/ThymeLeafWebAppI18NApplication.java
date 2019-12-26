67239b7ac0a601b8c172313ce22a486d5f15b9a5
Kishore Kumar <kishorekumar5464@gmail.com>
2019-12-26 23:21:32 +0530

    New Thymeleaf added I18N

---
+++ 23. ThymeLeaf/ThymeLeaf_Web_App_Internationalization/src/main/java/com/kishore/ThymeLeafWebAppI18NApplication.java
@@ -0,0 +1,13 @@
+package com.kishore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThymeLeafWebAppI18NApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ThymeLeafWebAppI18NApplication.class, args);
+	}
+
+}
